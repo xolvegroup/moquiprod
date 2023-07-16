5a19a05c3f33aee0cc87483756c659ada097eb7a
xolvegirindra <107811649+xolvegirindra@users.noreply.github.com>
2023-07-16 13:50:17 +0530

    github script file

---
+++ github-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+rm -rf moquiprod
+echo "download from repository"
+git clone https://github.com/xolvegroup/moquiprod.git moquiprod && cd moquiprod
+./gradlew getComponent -Pcomponent=HiveMind
+./gradlew downloadOpenSearch
+cd runtime/component
+git clone git@github.com:xolvegroup/xolve-theme.git
+git clone https://github.com/xolvegroup/WorkManagement.git
+git clone https://github.com/xolvegroup/Sales.git
+cd runtime/lib/
+wget https://jdbc.postgresql.org/download/postgresql-42.2.9.jar
+cd ../..
+./gradlew build
+./gradlew load
+./gradlew addRuntime
