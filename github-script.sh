1e48642a942a6719c4d9f5e4b2bfba897b553579
xolvegirindra <107811649+xolvegirindra@users.noreply.github.com>
2023-10-18 19:25:01 +0530

    Update github-script.sh

--- github-script.sh
+++ github-script.sh
@@ -10,7 +10,7 @@ cd runtime/component
 git clone https://github.com/xolvegroup/xolve-theme.git
 git clone https://github.com/xolvegroup/WorkManagement.git
 git clone https://github.com/xolvegroup/Sales.git
-curl -L https://jdbc.postgresql.org/download/postgresql-42.2.9.jar -o runtime/lib/postgresql-42.2.9.jar
+curl -L https://jdbc.postgresql.org/download/postgresql-42.2.9.jar -o /home/runner/work/moquiprod/moquiprod/runtime/lib/postgresql-42.2.9.jar
 cd ../..
 ./gradlew build
 #./gradlew load
